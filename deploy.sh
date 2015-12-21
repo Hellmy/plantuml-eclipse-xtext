1839b3ac825f78cc8b12ba466d3a97f760d8e610
Manuel Oddoy <manuel@zeunerds.de>
2015-12-21 14:56:30 +0100

    Update git push

--- deploy.sh
+++ deploy.sh
@@ -22,13 +22,9 @@ git add -A *
 echo "add done"
 git commit -m "Deploy to GitHub Pages"
 echo "commit done"
-echo $GH_REF
-echo "${GH_REF}"
-echo "$GH_REF"
-echo "echo done"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://$GH_TOKEN@$GH_REF" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
