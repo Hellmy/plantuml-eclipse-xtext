93c386fdaebd6180c1e4daa2e2ff0cb8e92b7c02
Manuel Oddoy <manuel@zeunerds.de>
2016-01-20 13:24:27 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git config user.email "<you>@<your-email>"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-git add -A *
+git add -A
 echo "add done"
 git commit -m "Deploy to GitHub Pages"
 echo "commit done"
